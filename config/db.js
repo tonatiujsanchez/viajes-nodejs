7d016b1ff56ff826902c3dc91387aa9fa90d35d3
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-06-14 14:19:34 -0500

    Variables de entorno implementadas

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 import Sequelize from 'sequelize'
+import dotenv from 'dotenv/config'
 
 
-const db = new Sequelize('agencia_viajes', 'root', 'root', {
-    host: '127.0.0.1',
+
+console.log(process.env.DB_HOST);
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
     define: {
